4324b5e6fcc03110599a9e152f9b92e19c558d3c
Mustafa Gemsiz <birhatgemsiz@hotmail.com>
2021-09-23 20:44:39 +0300

    update

--- OnlineTicariOtomasyon/Files/dist/assets/demo/datatables-demo.js
+++ OnlineTicariOtomasyon/Files/dist/assets/demo/datatables-demo.js
@@ -2,9 +2,12 @@
 $(document).ready(function () {
     $('.myDataTables').DataTable(
         {
+
             "language": {
                 "url": "//cdn.datatables.net/plug-ins/9dcbecd42ad/i18n/Turkish.json"
             }
+
         }
     );
+
 });
