e7278a9261720dcb9b9edfa46b2114ce7cb4c99e
Mustafa Gemsiz <birhatgemsiz@hotmail.com>
2021-09-23 18:29:36 +0300

    update

--- OnlineTicariOtomasyon/Controllers/SatisController.cs
+++ OnlineTicariOtomasyon/Controllers/SatisController.cs
@@ -9,6 +9,7 @@ namespace OnlineTicariOtomasyon.Controllers
 {
     public class SatisController : Controller
     {
+
         DataContext context = new DataContext();
         // GET: Satis
         public ActionResult Index()
@@ -80,6 +81,84 @@ namespace OnlineTicariOtomasyon.Controllers
             }
             return View(s);
 
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            ViewBag.urunAd = new SelectList(context.Uruns, "UrunId", "UrunAd");
+
+            var cari = context.Caris
+         .Select(x =>
+                 new
+                 {
+                     id = x.CariId,
+                     nameSurname = x.CariAd + " " + x.CariSoyad
+                 });
+
+            ViewBag.cariAdSoyad = new SelectList(cari, "id", "nameSurname");
+
+            var personel = context.Personels.Where(x => x.DepartmanId == 2)
+                     .Select(x =>
+                             new
+                             {
+                                 id = x.PersonelId,
+                                 nameSurname = x.PersonelAd + " " + x.PersonelSoyad
+                             });
+
+            ViewBag.personelAdSoyad = new SelectList(personel, "id", "nameSurname");
+
+            var deger = context.SatisHarekets.Find(id);
+            return View("Edit",deger);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(SatisHareket s)
+        {
+            ViewBag.urunAd = new SelectList(context.Uruns, "UrunId", "UrunAd");
+
+            var cari = context.Caris
+         .Select(x =>
+                 new
+                 {
+                     id = x.CariId,
+                     nameSurname = x.CariAd + " " + x.CariSoyad
+                 });
+
+            ViewBag.cariAdSoyad = new SelectList(cari, "id", "nameSurname");
+
+            var personel = context.Personels.Where(x => x.DepartmanId == 2)
+                     .Select(x =>
+                             new
+                             {
+                                 id = x.PersonelId,
+                                 nameSurname = x.PersonelAd + " " + x.PersonelSoyad
+                             });
+
+            ViewBag.personelAdSoyad = new SelectList(personel, "id", "nameSurname");
+
+            if (ModelState.IsValid)
+            {
+                var deger = context.SatisHarekets.Find(s.SatisId);
+                deger.UrunId = s.UrunId;
+                deger.CariId = s.CariId;
+                deger.PersonelId = s.PersonelId;
+                deger.Adet = s.Adet;
+                deger.Fiyat = s.Fiyat;
+                deger.ToplamTutar = s.ToplamTutar;
+                deger.Tarih = s.Tarih;
+                context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(s);
+        }
+
+
+        [HttpGet]
+        public ActionResult Detail(int id)
+        {
+            var deger = context.SatisHarekets.Where(x => x.SatisId == id).ToList();
+            return View(deger);
         }
     }
 }
