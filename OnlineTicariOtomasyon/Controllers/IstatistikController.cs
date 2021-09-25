c0de71ea5bfe3733286b32696c5b60578f0c7ca3
Mustafa Gemsiz <birhatgemsiz@hotmail.com>
2021-09-25 21:54:31 +0300

    update

---
+++ OnlineTicariOtomasyon/Controllers/IstatistikController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using OnlineTicariOtomasyon.Models.Model;
+namespace OnlineTicariOtomasyon.Controllers
+{
+    public class IstatistikController : Controller
+    {
+        DataContext context = new DataContext();
+        // GET: Istatistik
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
